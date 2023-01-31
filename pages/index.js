df585b408be64176b77ff3e70bea574431699413
ihzavip <ihzavip@gmail.com>
2023-01-31 21:46:58 +0700

    make it appropriate

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <h2 className="text-red-900 text-3xl">kontol</h2>
-      <h2 className="text-yellow-300 text-5xl">kontol</h2>
+      <h2 className="text-red-900 text-3xl">Hello world</h2>
     </>
   );
 }
